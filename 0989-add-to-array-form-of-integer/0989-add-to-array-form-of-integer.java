9eb568404fa5f3bcd2348c7d07e61a674f40dade
minseo  jo <liging12@naver.com>
2023-02-15 19:27:18 +0900

    Time: 29 ms (27.52%), Space: 44 MB (45.25%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,37 +1,15 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> temp = new ArrayList<Integer>();
-        while(k > 0) {
-            temp.add(k%10);
-            k/=10;
-        }
-        Collections.reverse(temp);
-        int[] num2 = temp.stream()
-        .mapToInt(Integer::intValue)
-        .toArray();
-
         List<Integer> result = new ArrayList<Integer>();
-        int i=num.length-1, j=num2.length-1;
-        int sum=0, carry=0;
-        while(i >= 0 || j >= 0) {
-            if(i>=0 && j>=0) sum = num[i] + num2[j];
-            if(i<0) sum = num2[j];
-            if(j<0) sum = num[i];
-            carry = sum/10;
-            sum = sum%10;
-            result.add(0, sum);
-            
-            if(i>j) {
-                if(i<=0) result.add(0, carry);
-                else num[i-1] += carry;
-            } else {
-                if(j<=0) result.add(0, carry);
-                else num2[j-1] += carry;
+        int len = num.length-1;
+        while(len >= 0 || k != 0){
+            if(len >= 0){
+                k += num[len--];
             }
-            if(i >= 0) i--;
-            if(j >= 0) j--;
+            
+            result.add(0, k % 10);
+            k /= 10;
         }
-        if(result.get(0) == 0) result.remove(0);
         return result;
     }
 }
